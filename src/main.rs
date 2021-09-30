e90a3a8d7baafc6b89de8ff1fdaea130fcb41567
Daniel Kiss <littlesnorrboy@gmail.com>
2021-09-30 21:35:09 +0200

    poggies

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,10 @@ use std::{mem::swap, time::Duration};
 use bevy::prelude::*;
 
 const RADIUS: f32 = 1.45;
+const VELOCITY: f32 = 2.;
 
-struct FromLatLong(pub LatLong);
-struct ToLatLong(pub LatLong);
-struct CurrentLatLong(pub LatLong);
+struct AnimFrom(pub Quat);
+struct AnimTo(pub Quat);
 struct AnimTimer(pub Timer);
 
 #[derive(Clone, Copy, Default)]
@@ -15,28 +15,13 @@ struct LatLong {
     pub long: f32,
 }
 
-fn latlong_to_cartesian(l: LatLong, r: f32) -> Vec3 {
-    let (latsin, latcos) = l.lat.sin_cos();
-    let (longsin, longcos) = l.long.sin_cos();
-
-    let x = r * latcos * longcos;
-    let y = r * latcos * longsin;
-    let z = r * latsin;
-    return Vec3::new(x, y, z);
-}
-
-fn lerp(a: f32, b: f32, t: f32) -> f32 {
-    a + (b - a) * t
+fn latlong_to_quat(l: LatLong) -> Quat {
+    Quat::from_axis_angle(Vec3::X, l.lat) * Quat::from_axis_angle(Vec3::Y, l.long)
 }
 
 fn update_latlong_system(
     t: Res<Time>,
-    mut q: Query<(
-        &mut CurrentLatLong,
-        &mut FromLatLong,
-        &mut ToLatLong,
-        &mut AnimTimer,
-    )>,
+    mut q: Query<(&mut Transform, &mut AnimFrom, &mut AnimTo, &mut AnimTimer)>,
 ) {
     let delta = t.delta();
     for (mut current, mut from, mut to, mut t) in q.iter_mut() {
@@ -49,16 +34,7 @@ fn update_latlong_system(
 
         let t = t.0.percent();
 
-        current.0.lat = lerp(from.0.lat, to.0.lat, t);
-        current.0.long = lerp(from.0.long, to.0.long, t);
-    }
-}
-
-fn update_transforms_system(mut q: Query<(&mut Transform, &CurrentLatLong)>) {
-    let r = RADIUS + 0.2;
-
-    for (mut tr, curr) in q.iter_mut() {
-        tr.translation = latlong_to_cartesian(curr.0, r);
+        current.rotation = from.0.slerp(to.0, t);
     }
 }
 
@@ -67,25 +43,94 @@ fn setup_planes_system(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    cmd.spawn_bundle(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Cube { size: 0.2 })),
-        material: materials.add(StandardMaterial {
-            base_color: Color::YELLOW,
-            metallic: 0.3,
-            unlit: false,
-            ..Default::default()
-        }),
-        ..Default::default()
-    })
-    .insert_bundle((
-        FromLatLong(LatLong { lat: 0., long: 0. }),
-        ToLatLong(LatLong {
-            lat: 3.141592,
-            long: 3.141592 / 2.,
-        }),
-        CurrentLatLong(LatLong::default()),
-        AnimTimer(Timer::new(Duration::from_secs(5), true)),
-    ));
+    const PATHS: &[[LatLong; 2]] = &[
+        [
+            LatLong {
+                lat: -0.2,
+                long: 1.3,
+            },
+            LatLong {
+                lat: 3.141592 / 4.,
+                long: -3.141592 / 3.,
+            },
+        ],
+        [
+            LatLong {
+                lat: 0.,
+                long: -0.2,
+            },
+            LatLong {
+                lat: 3.14 / 5.,
+                long: -3.14 / 8.,
+            },
+        ],
+    ];
+
+    const PLANE_RADIUS: f32 = RADIUS + 0.2;
+
+    for path in PATHS {
+        let [from, to] = *path;
+        // spawn our 'plane'
+        {
+            let mut from = latlong_to_quat(from);
+            let to = latlong_to_quat(to);
+            let d = from.dot(to);
+            if d < 0. {
+                // choose the shortest path for interpolation
+                // Quat doesn't have mul (f32) operator...
+                let [x, y, z, w] = from.as_mut();
+                *x *= -1.;
+                *y *= -1.;
+                *z *= -1.;
+                *w *= -1.;
+            }
+            let t = from.angle_between(to) * PLANE_RADIUS / VELOCITY;
+            cmd.spawn_bundle((
+                AnimFrom(from),
+                AnimTo(to),
+                AnimTimer(Timer::new(Duration::from_secs_f32(t), true)),
+                Transform::default(),
+                GlobalTransform::default(),
+            ))
+            .with_children(|chld| {
+                chld.spawn_bundle(PbrBundle {
+                    mesh: meshes.add(Mesh::from(shape::Cube { size: 0.12 })),
+                    material: materials.add(StandardMaterial {
+                        base_color: Color::YELLOW,
+                        metallic: 0.3,
+                        unlit: false,
+                        ..Default::default()
+                    }),
+                    transform: Transform::from_translation(Vec3::Z * (PLANE_RADIUS)),
+                    ..Default::default()
+                });
+            });
+        }
+
+        // destinations
+        for point in [from, to] {
+            cmd.spawn_bundle((
+                Transform::from_rotation(latlong_to_quat(point)),
+                GlobalTransform::default(),
+            ))
+            .with_children(|chld| {
+                chld.spawn_bundle(PbrBundle {
+                    mesh: meshes.add(Mesh::from(shape::Icosphere {
+                        radius: 0.05,
+                        subdivisions: 16,
+                    })),
+                    material: materials.add(StandardMaterial {
+                        base_color: Color::RED,
+                        metallic: 0.3,
+                        unlit: false,
+                        ..Default::default()
+                    }),
+                    transform: Transform::from_translation(Vec3::Z * (PLANE_RADIUS)),
+                    ..Default::default()
+                });
+            });
+        }
+    }
 }
 
 fn setup_system(
@@ -97,23 +142,28 @@ fn setup_system(
     cmd.spawn_bundle(PbrBundle {
         mesh: meshes.add(Mesh::from(shape::Icosphere {
             radius: RADIUS,
-            subdivisions: 32,
+            subdivisions: 64,
         })),
         material: materials.add(StandardMaterial {
             base_color: Color::BLUE,
-            metallic: 0.3,
+            metallic: 0.8,
             unlit: false,
             ..Default::default()
         }),
         transform: Transform::from_translation(Vec3::ZERO),
         ..Default::default()
     });
+    // light
+    cmd.spawn_bundle(LightBundle {
+        transform: Transform::from_translation(Vec3::new(0.0, 5.0, 5.0)),
+        ..Default::default()
+    });
     // camera
     cmd.spawn_bundle(OrthographicCameraBundle {
-        transform: Transform::from_translation(Vec3::new(2.0, 0.0, 2.0))
+        transform: Transform::from_translation(Vec3::new(0.0, 0.0, 2.0))
             .looking_at(Vec3::ZERO, Vec3::Y),
         orthographic_projection: bevy::render::camera::OrthographicProjection {
-            scale: 0.01,
+            scale: 0.005,
             ..Default::default()
         },
         ..OrthographicCameraBundle::new_3d()
@@ -126,6 +176,5 @@ fn main() {
         .add_startup_system(setup_system.system())
         .add_startup_system(setup_planes_system.system())
         .add_system(update_latlong_system.system())
-        .add_system(update_transforms_system.system())
         .run();
 }
